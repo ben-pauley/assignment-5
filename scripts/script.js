196704836c3814d761d98468f4ff343c7b7a70b5
Ben <pauleybj@gmail.com>
2021-01-13 22:51:09 +0000

    made event divs editable

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,5 @@
-// In tag with ID "currentDay" should display the current day
-
-// html should display 9 different blocks, one for each hour of the day (9 am to 5pm)
-// --each block should have the 3 blocks of their own
-// ----hour, e.g. 10am
-// ------white background
-// ----space to write an event
-// ----save button
-// ------button with blue background with save icon
-//--the current hour should have its event text portion highlighted red
-//--all previous hours should be grey, future hours should be green
+//the current hour should have its event text portion highlighted red
+//all previous hours should be grey, future hours should be green
 
 // user should be able to click in any time block and change the text
 // when save button is clicked, updated text should be stored in local storage
@@ -23,11 +14,18 @@
 // if save button has been pressed, data should be saved to local storage
 // function that takes data from local storage and outputs it to html whenever page is loaded
 
+$(document).ready(function () {
+  $("#currentDay").text(todaysDate());
+  $(".saveBtn").click(function () {
+    storeEventDetails();
+  });
+});
+
 function todaysDate() {
   var today = moment().format("dddd, MMMM Do");
   return today;
 }
 
-$(document).ready(function () {
-  $("#currentDay").text(todaysDate());
-});
+function storeEventDetails() {
+  console.log("test");
+}
