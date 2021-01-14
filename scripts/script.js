1f3f760c3b85e16a9b69879daf70591abc610438
Ben <pauleybj@gmail.com>
2021-01-14 13:28:25 +0000

    attempted to make function buttonListeners more DRY, unsuccessful but kept attempt commented out, removed comments that are no longer needed

--- scripts/script.js
+++ scripts/script.js
@@ -1,52 +1,50 @@
-//the current hour should have its event text portion highlighted red
-//all previous hours should be grey, future hours should be green
-
-// user should be able to click in any time block and change the text
-// when save button is clicked, updated text should be stored in local storage
-// whenever the page is loaded, all data from local storage should be displayed in time blocks
-// there is a lot of styling for the time blocks already in the css file!
-
-//====================================================
-// PSEUDOCODE
-
-// variable that stores the current day
-// function that checks if save button has been pressed
-// if save button has been pressed, data should be saved to local storage
-// function that takes data from local storage and outputs it to html whenever page is loaded
-
 $(document).ready(function () {
   $("#currentDay").text(todaysDate());
 
   loadEventContent();
   applyCSS();
+  buttonListeners();
 
-  $("#btn9").click(function () {
-    storeEventContent("#event9", "event9");
-  });
-  $("#btn10").click(function () {
-    storeEventContent("#event10", "event10");
-  });
-  $("#btn11").click(function () {
-    storeEventContent("#event11", "event11");
-  });
-  $("#btn12").click(function () {
-    storeEventContent("#event12", "event12");
-  });
-  $("#btn13").click(function () {
-    storeEventContent("#event13", "event13");
-  });
-  $("#btn14").click(function () {
-    storeEventContent("#event14", "event14");
-  });
-  $("#btn15").click(function () {
-    storeEventContent("#event15", "event15");
-  });
-  $("#btn16").click(function () {
-    storeEventContent("#event16", "event16");
-  });
-  $("#btn17").click(function () {
-    storeEventContent("#event17", "event17");
-  });
+  function buttonListeners() {
+    // FAILED ATTEMPT AT MAKING CODE MORE DRY:
+
+    // for (var i = 9; i <= 17; i++) {
+    //   var btnID = "#btn" + i;
+    //   var eventID = "#event" + i;
+    //   var localStorageKey = "event" + i;
+    //   $(btnID).click(function () {
+    //     storeEventContent(eventID, localStorageKey);
+    //   });
+    // }
+
+    $("#btn9").click(function () {
+      storeEventContent("#event9", "event9");
+    });
+    $("#btn10").click(function () {
+      storeEventContent("#event10", "event10");
+    });
+    $("#btn11").click(function () {
+      storeEventContent("#event11", "event11");
+    });
+    $("#btn12").click(function () {
+      storeEventContent("#event12", "event12");
+    });
+    $("#btn13").click(function () {
+      storeEventContent("#event13", "event13");
+    });
+    $("#btn14").click(function () {
+      storeEventContent("#event14", "event14");
+    });
+    $("#btn15").click(function () {
+      storeEventContent("#event15", "event15");
+    });
+    $("#btn16").click(function () {
+      storeEventContent("#event16", "event16");
+    });
+    $("#btn17").click(function () {
+      storeEventContent("#event17", "event17");
+    });
+  }
 
   function todaysDate() {
     var today = moment().format("dddd, MMMM Do");
