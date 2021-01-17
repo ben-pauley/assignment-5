479decf1747ac36dae848d4d8f75d3dfc5954d22
Ben <pauleybj@gmail.com>
2021-01-17 17:33:31 +0000

    removed unnecessary comments and improved code layout

--- scripts/script.js
+++ scripts/script.js
@@ -10,50 +10,47 @@ $(document).ready(function () {
 
     // for (var i = 9; i <= 17; i++) {
     //   var btnID = "#btn" + i;
-    //   var eventID = "#event" + i;
-    //   var localStorageKey = "event" + i;
+    //   var eventNum = "event" + i;
     //   $(btnID).click(function () {
-    //     storeEventContent(eventID, localStorageKey);
+    //     storeEventContent(eventNum);
     //   });
     // }
 
     $("#btn9").click(function () {
-      storeEventContent("#event9", "event9");
+      storeEventContent("event9");
     });
     $("#btn10").click(function () {
-      storeEventContent("#event10", "event10");
+      storeEventContent("event10");
     });
     $("#btn11").click(function () {
-      storeEventContent("#event11", "event11");
+      storeEventContent("event11");
     });
     $("#btn12").click(function () {
-      storeEventContent("#event12", "event12");
+      storeEventContent("event12");
     });
     $("#btn13").click(function () {
-      storeEventContent("#event13", "event13");
+      storeEventContent("event13");
     });
     $("#btn14").click(function () {
-      storeEventContent("#event14", "event14");
+      storeEventContent("event14");
     });
     $("#btn15").click(function () {
-      storeEventContent("#event15", "event15");
+      storeEventContent("event15");
     });
     $("#btn16").click(function () {
-      storeEventContent("#event16", "event16");
+      storeEventContent("event16");
     });
     $("#btn17").click(function () {
-      storeEventContent("#event17", "event17");
+      storeEventContent("event17");
     });
   }
 
   function todaysDate() {
-    var today = moment().format("dddd, MMMM Do");
-    return today;
+    return moment().format("dddd, MMMM Do");
   }
 
-  function storeEventContent(eventID, localStorageKey) {
-    var eventContent = $(eventID).text();
-    localStorage.setItem(localStorageKey, eventContent);
+  function storeEventContent(eventNum) {
+    localStorage.setItem(eventNum, $("#" + eventNum).text());
   }
 
   function loadEventContent() {
@@ -67,15 +64,15 @@ $(document).ready(function () {
   }
 
   function applyCSS() {
-    var currentTime = moment().format("HH:mm:ss");
     for (var i = 9; i <= 17; i++) {
       var eventID = "#event" + i;
       var eventValue = $(eventID).attr("value");
-      if (moment().hour(i).isSame(moment())) {
+      var hourBlock = moment().hour(i);
+      if (hourBlock.isSame(moment())) {
         $(eventID).addClass("present");
-      } else if (moment().hour(i).isBefore(moment())) {
+      } else if (hourBlock.isBefore(moment())) {
         $(eventID).addClass("past");
-      } else if (moment().hour(i).isAfter(moment())) {
+      } else if (hourBlock.isAfter(moment())) {
         $(eventID).addClass("future");
       }
     }
